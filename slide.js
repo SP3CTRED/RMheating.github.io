577053d5ec9c1a7a7ec886692082206c1fd1d833
SP3CTRED <104116277+SP3CTRED@users.noreply.github.com>
2023-07-10 17:02:15 -0600

    new code

---
+++ slide.js
@@ -0,0 +1,10 @@
+// You can use a JavaScript library like Swiper or Slick to create the slider functionality.
+// Here's a basic example using Swiper library:
+
+var mySwiper = new Swiper('.slider', {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+  });
+  
